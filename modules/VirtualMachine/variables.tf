fd763ea1e059f61bdfc23662f36f2fb98fa5c93c
Chayan Deokar <74093567+chayandeokar@users.noreply.github.com>
2023-12-11 12:01:08 +0530

    Add files via upload

---
+++ modules/VirtualMachine/variables.tf
@@ -0,0 +1,10 @@
+variable "ami" {
+  description = "ami-02a2af70a66af6dfb"
+}
+
+variable "instance_type" {
+  description = "t2.micro"
+}
+variable "subnet_id"{
+  description = "aws_subnet.my_subnet.id"
+}
