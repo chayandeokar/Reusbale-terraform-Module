d281112c4b9a3d3392227c72c840a6f34af23ba9
Chayan Deokar <74093567+chayandeokar@users.noreply.github.com>
2023-12-12 10:47:48 +0530

    Add files via upload

---
+++ env/development/ec2-tech-test/variables.tf
@@ -0,0 +1,9 @@
+variable "instance_type" {
+  description = "EC2 instance type"
+  // Define other variables as needed
+}
+
+variable "lb_name" {
+  description = "Load Balancer name"
+  // Define other variables as needed
+}
